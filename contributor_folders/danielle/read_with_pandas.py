018221883efd7d1f4ed42650fcf896ff1b70f542
Danielle Dempsey <ddempsey@perennia.ca>
2026-08-25 19:47:01 -0300

    simplify for digby data

--- contributor_folders/danielle/read_with_pandas.py
+++ contributor_folders/danielle/read_with_pandas.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 county_resource_codes = {"Digby": "wpsu-7fer"}
 
+output_file = "downloaded_data.csv"
+
 for county, resource_code in county_resource_codes.items():
     url = f"https://data.novascotia.ca/resource/{resource_code}.csv"
     df = pd.read_csv(url)
-    col = "timestamp_utc"
-    timestamp_na_count = df[col].isna().sum()
+    df.to_csv(output_file, index=False)
 
-    print(f"The {county} dataset has {timestamp_na_count} NA values in the {col} column.")
